5fb0d3e2725e3f73800f3e6e590c8dde0feee29d
nuremberggroupjo-ship-it <nuremberggroupjo@gmail.com>
2025-12-07 11:02:13 +0300

    studioone constant

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,5 @@
-export const AppName = process.env.NEXT_PUBLIC_APP_NAME || "Jordan Ranger";
+export const AppName = process.env.NEXT_PUBLIC_APP_NAME || "Studio One";
 export const AppDescription =
-  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Jordan Ranger";
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Studio One";
 export const AppURL =
   process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
